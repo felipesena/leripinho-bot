38d5e025ce4241efef8170cf418285a242d01be3
Felipe Sena <felipedepaulasena@hotmail.com>
2018-08-24 21:23:16 -0300

    Procfile

--- bot.py
+++ bot.py
@@ -2,4 +2,9 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='$')
 
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say("Quita que tá lento!!")
+
+
 bot.run('NDgyMzM5NDE2NTc3NDA5MDI0.DmIp_Q.Ex35Kr8K5bC2txZ4apbX28hEbO0')
